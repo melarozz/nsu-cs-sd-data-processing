8df973df5ae475f7325818bc03ff34e273209a6e
melarozz <melarozz18@gmail.com>
2024-09-15 18:13:30 +0700

    Task 3 fix

--- JavaConcurrency/Task3/Main.java
+++ JavaConcurrency/Task3/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
         System.out.println("Main thread starts");
 
-        String[] sequence1 = {"Thread 1 - Line 1", "Thread 1 - Line 2", "Thread 1 - Line 3"};
-        String[] sequence2 = {"Thread 2 - Line 1", "Thread 2 - Line 2", "Thread 2 - Line 3"};
-        String[] sequence3 = {"Thread 3 - Line 1", "Thread 3 - Line 2", "Thread 3 - Line 3"};
-        String[] sequence4 = {"Thread 4 - Line 1", "Thread 4 - Line 2", "Thread 4 - Line 3"};
+        String[] sequence1 = {"Line 1 ", "Line 2 ", "Line 3 "};
 
         // creating 4 Thread objects, start eaach of them, the run() method executes in each of them
         // all threads will work in parallel
-        Thread thread1 = new Thread(new MyRunnable(sequence1));
-        Thread thread2 = new Thread(new MyRunnable(sequence2));
-        Thread thread3 = new Thread(new MyRunnable(sequence3));
-        Thread thread4 = new Thread(new MyRunnable(sequence4));
+        Thread thread1 = new Thread(new MyRunnable(sequence1.clone(), 1));
+        sequence1[0]="Hello 1";
+        Thread thread2 = new Thread(new MyRunnable(sequence1.clone(), 2));
+        sequence1[0]="Hello 2";
+        Thread thread3 = new Thread(new MyRunnable(sequence1.clone(), 3));
+        sequence1[0]="Hello 3";
+        Thread thread4 = new Thread(new MyRunnable(sequence1.clone(), 4));
 
         thread1.start();
         thread2.start();
@@ -30,15 +30,17 @@ public class Main {
 class MyRunnable implements Runnable {
 
     private final String[] sequence;
+    private final Integer number;
 
-    public MyRunnable(String[] sequence) {
+    public MyRunnable(String[] sequence, Integer number) {
         this.sequence = sequence;
+        this.number = number;
     }
 
     @Override
     public void run() {
         for (String line : sequence) {
-            System.out.println(line);
+            System.out.println(line + " thread " + number.toString());
         }
     }
 }
