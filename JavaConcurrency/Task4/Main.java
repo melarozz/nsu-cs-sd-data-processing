964383f811ebc8a315297e78eb34c2a5cf149457
melarozz <melarozz18@gmail.com>
2024-09-26 12:43:17 +0700

    Small fixes

--- JavaConcurrency/Task4/Main.java
+++ JavaConcurrency/Task4/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main thread starts");
 
-        Thread childThread = new Thread(new MyRunnable());
+        Thread childThread = new Thread(new StringPrinterRunnable());
         childThread.start();
 
         // main thread 2 sec sleep before interrupting child
@@ -22,13 +22,15 @@ public class Main {
     }
 }
 
-class MyRunnable implements Runnable {
+class StringPrinterRunnable implements Runnable {
+
+    private static final int NUM_ITERATIONS = 1000;
 
     @Override
     public void run() {
         System.out.println("Child thread starts");
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
 
             System.out.println("Child thread: Line #" + i);
 
